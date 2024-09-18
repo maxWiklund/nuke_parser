38a17df2a12d78a1f60789ef7e49cf0ca3a2d6d1
Max Wiklund <info@maxwiklund.com>
2024-09-18 12:12:56 +0200

    Update version.

--- parser/nuke_parser/__init__.py
+++ parser/nuke_parser/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 __import__("pkg_resources").declare_namespace(__name__)
-__version__ = "0.1.1"
+__version__ = "0.2.0"
